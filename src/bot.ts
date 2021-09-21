d9a739754b012ef5d44e94ff109e3dbfd3ab461f
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2021-09-20 21:01:22 -0300

    Refact player module and commands select

--- src/bot.ts
+++ src/bot.ts
@@ -5,7 +5,7 @@ import Player from './modules/play';
 
 import { prefix } from './metadata.json';
 import { isInvalidMessage } from './utils/functions';
-import {getBotMessage} from './utils/botMessages';
+import { getMessage } from './utils/botMessages';
 
 const client = new Discord.Client();
 const musicPlayer = new Player();
@@ -25,24 +25,9 @@ client.once('disconnect', () => {
 client.on('message', async (message) => {
   if (isInvalidMessage(message)) return;
 
-  const {content, channel} = message;
-  const userCmd = content.split(/ (.+)/)[0];
+  const response = musicPlayer.handleUserCommand(message, prefix);
 
-  if ([`${prefix}play`, `${prefix}p`].includes(userCmd)) {
-    musicPlayer.play(message);
-  } else if ([`${prefix}skip`, `${prefix}skp`].includes(userCmd)) {
-    musicPlayer.skip(message);
-  } else if ([`${prefix}stop`, `${prefix}stp`].includes(userCmd)) {
-    musicPlayer.stop(message);
-  } else if ([`${prefix}queue`, `${prefix}qeue`, `${prefix}queu`].includes(userCmd)) {
-    musicPlayer.queue(message);
-  } else if ([`${prefix}remove`, `${prefix}rmv`].includes(userCmd)) {
-    const position = content.split(/ (.+)/)[1];
-    if (!position) return channel.send('You must provide song position to remove');
-    musicPlayer.remove(message, parseInt(position));
-  } else {
-    channel.send(getBotMessage('COMMAND_NOT_FOUND'));
-  }
+  if(!response) return message.channel.send(getMessage('COMMAND_NOT_FOUND'));
 });
 
 client.login(process.env.BOT_TOKEN);
